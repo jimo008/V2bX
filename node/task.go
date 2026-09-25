f287d7bfa2528c505e0909942d43c7211a276622
Huang Huang <huanghuang@HuangdeMacBook-Air.local>
2026-09-25 23:10:11 +0800

    Fix empty user list sync handling

--- node/task.go
+++ node/task.go
@@ -172,7 +172,7 @@ func (c *Controller) nodeInfoMonitor() (err error) {
 		c.limiter.AliveList = newA
 	}
 	// node no changed, check users
-	if len(newU) == 0 {
+	if newU == nil {
 		return nil
 	}
 	deleted, added := compareUserList(c.userList, newU)
